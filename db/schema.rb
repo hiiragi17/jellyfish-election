1b333dd72e0953f982882fb1cd0e647e403ede90
hiiragi17x <hiiragi.en22@gmail.com>
2022-05-20 11:51:48 +0900

    マイグレ

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_17_043947) do
+ActiveRecord::Schema.define(version: 2022_05_19_080614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.bigint "jellyfish_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["jellyfish_id"], name: "index_comments_on_jellyfish_id"
+  end
 
   create_table "jellyfishes", force: :cascade do |t|
     t.string "title"
@@ -42,5 +50,6 @@ ActiveRecord::Schema.define(version: 2022_05_17_043947) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "comments", "jellyfishes"
   add_foreign_key "likes", "jellyfishes"
 end
