5279f6ac7c86e21524477d89bb73974e0b934464
hiiragi17x <hiiragi.en22@gmail.com>
2022-05-20 15:18:17 +0900

    修正

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -1,5 +1,4 @@
 class Admin::UserSessionsController < Admin::BaseController 
-  skip_before_action :require_login, only: %i[new create]
   skip_before_action :check_admin, only: %i[new create]
    # ↓ログインページ用のレイアウトを用意するので宣言
   layout 'admin/layouts/admin_login'
